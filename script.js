13acbd9d21cb5722fc745452375abfc764569b97
Rui Perpetuo <ruiperpetuo@MacBook-Pro-de-Rui.local>
2022-07-12 17:36:05 +0100

    add amount validation

--- script.js
+++ script.js
@@ -7,16 +7,24 @@
  */
 function compute()
 {
-    var principal = parseFloat(document.getElementById("principal").value);
-    var rate = parseFloat(document.getElementById("rate").value);
-    var years = parseFloat(document.getElementById("years").value);
-    var result = principal * years * rate /100;
-    var years_modified = new Date().getFullYear() + parseInt(years);
+    if (document.getElementById("principal").value.length == 0)
+    {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    } 
+    else
+    {
+        var principal = parseFloat(document.getElementById("principal").value);
+        var rate = parseFloat(document.getElementById("rate").value);
+        var years = parseFloat(document.getElementById("years").value);
+        var result = principal * years * rate /100;
+        var years_modified = new Date().getFullYear() + parseInt(years);
 
-    document.getElementById("result").innerHTML = "If you deposit <mark>"
-   + principal + "</mark>, <br/> at an interest rate of <mark>"
-   + rate +"%.</mark><br/> You will receive an amount of <mark>" + result +
-   "</mark>,<br/>in the year <mark>" + years_modified + ".</mark><br/>";
+        document.getElementById("result").innerHTML = "If you deposit <mark>"
+        + principal + "</mark>, <br/> at an interest rate of <mark>"
+        + rate +"%.</mark><br/> You will receive an amount of <mark>" + result +
+        "</mark>,<br/>in the year <mark>" + years_modified + ".</mark><br/>";
+    }
 }
 
 /*
@@ -37,6 +45,7 @@ function updateRate()
 function checkAmount() 
 {
     var principal = document.getElementById("principal").value;
+
     if (principal <= 0) 
     {
         alert("Enter a non-zero or a positive number");
